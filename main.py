b46af2a3da4ed936616c62dfc76e059133b3abd7
Jomelgar <jomelgar1903@gmail.com>
2026-03-02 19:43:28 -0600

    args for the python main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import sys
 from neuralnetwork import NeuralNet
 import numpy as np
 import matplotlib.pyplot as plt
 
 file = "./json/mnist_mlp_pretty.json"
-data_url = "./assets/mnist_custom_ds.npz"
+if not isinstance(sys.argv[1],str):
+    raise ValueError("The file must be a str for direction")
 
 network = NeuralNet(file=file)
-data = np.load(data_url)
+data = np.load(sys.argv[1])
 images = data["images"].reshape(10,-1)/255
 
 results = network.solve_batch(images)
